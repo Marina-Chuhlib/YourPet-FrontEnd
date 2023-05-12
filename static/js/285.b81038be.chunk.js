3079cceaa9fa1149f1feacf47eda28f85308944c
Marina-Chuhlib <107684222+Marina-Chuhlib@users.noreply.github.com>
2023-05-12 19:20:03 +0000

    Deploying to gh-pages from @ Marina-Chuhlib/YourPet-FrontEnd@61ac157b82a028162c437d1e57918a3bd4d23d76 🚀

--- static/js/285.b81038be.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyour_pet_frontend=self.webpackChunkyour_pet_frontend||[]).push([[285],{285:function(n,e,r){r.r(e),r.d(e,{default:function(){return c}});var t=r(184),u=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)("h2",{children:"Home"})})},c=function(){return(0,t.jsx)(t.Fragment,{children:(0,t.jsx)(u,{})})}}}]);
-//# sourceMappingURL=285.b81038be.chunk.js.map
--- static/js/50.c6b54b7c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyour_pet_frontend=self.webpackChunkyour_pet_frontend||[]).push([[50],{50:function(n,e,r){r.r(e);var t=r(689),u=r(184);e.default=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)("main",{children:(0,u.jsx)("div",{children:(0,u.jsx)(t.j3,{})})})})}}}]);
-//# sourceMappingURL=50.c6b54b7c.chunk.js.map
