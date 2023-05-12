fc4b1d32372b1c8d0007bf71b0638826964aab44
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-05-12 15:36:54 +0300

    Update Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from 'modules/Header/Header';
 
-// const Layout = lazy(() => import('./modules/Layout/Layout'));
-const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
-const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const Layout = lazy(() => import('./modules/Layout/Layout'));
 const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
-const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
-const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
-const AddPetPage = lazy(() => import('./pages/AddPetPage/AddPetPage'));
+// const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
+// const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+// const NoticesPage = lazy(() => import('./pages/NoticesPage/NoticesPage'));
+// const UserPage = lazy(() => import('./pages/UserPage/UserPage'));
+// const AddPetPage = lazy(() => import('./pages/AddPetPage/AddPetPage'));
 
 export const App = () => {
   return (
@@ -18,14 +18,10 @@ export const App = () => {
         <Header></Header>
         <Suspense>
           <Routes>
-            <Route></Route>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<MainPage />} />
+            </Route>
           </Routes>
-          <RegisterPage></RegisterPage>
-          <LoginPage></LoginPage>
-          <MainPage></MainPage>
-          <NoticesPage></NoticesPage>
-          <UserPage></UserPage>
-          <AddPetPage></AddPetPage>
         </Suspense>
       </BrowserRouter>
     </>
