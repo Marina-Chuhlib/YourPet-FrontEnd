90c054b78aa5c32ce9c24766f563028877de2d9a
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-05-13 22:33:07 +0300

    update

--- src/modules/UserForm/UserForm.jsx
+++ src/modules/UserForm/UserForm.jsx
@@ -1,94 +1,106 @@
 import React from 'react';
-import { useFormik } from 'formik';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+
+import TextField from '@mui/material/TextField';
 
 import css from './UserForm.module.css';
 
-const UserSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  phone: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-});
+// const SignupSchema = Yup.object().shape({
+//   firstName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   lastName: Yup.string()
+//     .min(2, 'Too Short!')
+//     .max(50, 'Too Long!')
+//     .required('Required'),
+//   email: Yup.string().email('Invalid email').required('Required'),
+// });
 
 const UserForm = () => {
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
-        <h2 className={css.userInfo}>My information:</h2>
-    
-          <Formik
-            initialValues={{
-              name: '',
-              email: '',
-              phone: '',
-              birthday: '',
-              city: '',
-            }}
-            // validationSchema={UserSchema}
-            onSubmit={values => {
-              // same shape as initial values
-              console.log(values);
-            }}
-          >
+        <h2 className={css.title}>My information:</h2>
+
+        <Formik
+          initialValues={{
+            name: 'Max',
+            email: 'max@gmai.com',
+            birthday: '',
+            phone: '',
+            city: '',
+          }}
+          // validationSchema={SignupSchema}
+          onSubmit={values => {
+            // same shape as initial values
+            console.log(values);
+          }}
+        >
           {({ errors, touched }) => (
-                <div className={css.wrapperForm}>
-              <Form className={css.form}>
-           
-                  <label className={css.label}>
-                    Name <Field name="name" className={css.input} />
-                    {errors.firstName && touched.firstName ? (
-                      <div>{errors.firstName}</div>
-                    ) : null}
-                  </label>
-           
-
-                <label className={css.label}>
-                  Email
-                  <Field name="email" type="email" className={css.input} />
-                  {errors.email && touched.email ? (
-                    <div>{errors.email}</div>
-                  ) : null}
+            <Form className={css.forma}>
+              <img src="" alt="" className={css.avatar} />
+              <div>
+                <label htmlFor="editPhoto" className={css.avatarL}>
+                  Edit photo
+                  <input
+                    type="file"
+                    id="editPhoto"
+                    accept="image/*"
+                    name="Edit photo"
+                    className={css.avatarBtn}
+                  />
                 </label>
-
-                <label className={css.label}>
-                  Phone
-                  <Field name="phone" className={css.input} />
-                  {errors.phone && touched.lastName ? (
-                    <div>{errors.phone}</div>
-                  ) : null}
-                </label>
-
-                <label className={css.label}>
-                  Birthday
-                  <Field name="birthday" className={css.input} />
-                  {errors.birthday && touched.birthday ? (
-                    <div>{errors.birthday}</div>
-                  ) : null}
-                </label>
-
-                <label className={css.label}>
-                  City
-                  <Field name="city" className={css.input} />
-                  {errors.city && touched.city ? (
-                    <div>{errors.city}</div>
-                  ) : null}
-                </label>
-
-                <button type="submit">Submit</button>
-              </Form>
+                <svg className={css.icon}>
+                  <use href="./images/camera.svg"></use>
+                </svg>
               </div>
-            )}
-          </Formik>
-        </div>
-      </div>
 
+              <div className={css.row}>
+                <label className={css.label}>Name:</label>
+                <Field name="name" type="name" className={css.input} />
+                {errors.name && touched.name ? <div>{errors.name}</div> : null}
+              </div>
+
+              <div className={css.row}>
+                <label>Email:</label>
+                <Field name="email" type="email" className={css.input} />
+                {errors.email && touched.email ? (
+                  <div>{errors.email}</div>
+                ) : null}
+              </div>
+
+              <div className={css.row}>
+                <label>Birthday:</label>
+                <Field name="birthday" className={css.input} />
+                {errors.birthday && touched.birthday ? (
+                  <div>{errors.birthday}</div>
+                ) : null}
+              </div>
+
+              <div className={css.row}>
+                <label>Phone:</label>
+                <Field name="phone" className={css.input} />
+                {errors.phone && touched.phone ? (
+                  <div>{errors.phone}</div>
+                ) : null}
+              </div>
+
+              <div className={css.row}>
+                <label>City:</label>
+                <Field name="city" className={css.input} />
+                {errors.city && touched.city ? <div>{errors.city}</div> : null}
+              </div>
+
+              <button type="submit" className={css.Btn}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 export default UserForm;
