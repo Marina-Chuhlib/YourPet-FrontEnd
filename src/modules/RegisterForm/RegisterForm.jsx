c186a236de3cbe399b08cdf14508e406030bb67e
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-05-13 14:58:51 +0300

    Update RegisterForm

--- src/modules/RegisterForm/RegisterForm.jsx
+++ src/modules/RegisterForm/RegisterForm.jsx
@@ -1,18 +1,9 @@
-import { useState } from 'react';
-
 import { RegisterFormDetails } from './RegisterFormDetails';
 
 const RegisterForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
-
   return (
     <>
-      <RegisterFormDetails data={formData} />
+      <RegisterFormDetails />
     </>
   );
 };
