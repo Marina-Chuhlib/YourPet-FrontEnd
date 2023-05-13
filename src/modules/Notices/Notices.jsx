a74aa2d15d7117a9d1519c7b10043560cd0c231a
Tatiana-Hela <malya9472@gmail.com>
2023-05-13 19:44:24 +0300

    update search-input

--- src/modules/Notices/Notices.jsx
+++ src/modules/Notices/Notices.jsx
@@ -1,27 +1,29 @@
 import React, { useState } from 'react';
-import {  InputAdornment, IconButton,Input } from '@mui/material';
+import { InputAdornment, IconButton, Input } from '@mui/material';
 import { Search } from '@mui/icons-material';
+
+import css from '../Notices/Notices.module.css';
 
 const Notices = () => {
   const [keyword, setKeyword] = useState('');
-  
+
   // const SearchInput = () => {
   //   const [keyword, setKeyword] = useState('');
 
-    const handleSearch = () => {
-      // Здесь можно выполнить действия при нажатии на кнопку поиска, например, отправить запрос на сервер
-      console.log('Выполняется поиск по ключевому слову:', keyword);
-    };
+  const handleSearch = () => {
+    // Здесь можно выполнить действия при нажатии на кнопку поиска, например, отправить запрос на сервер
+    console.log('Выполняется поиск по ключевому слову:', keyword);
+  };
 
-    const handleChange = (event) => {
-      setKeyword(event.target.value);
-    };
+  const handleChange = event => {
+    setKeyword(event.target.value);
+  };
 
-    return (
-      <>
-        <h2>Find your favorite pet</h2>
-        <div>
-          <Input
+  return (
+    <>
+      <h2 className={css.title}>Find your favorite pet</h2>
+      <div className={css.inputContainer}>
+        <Input
           value={keyword}
           onChange={handleChange}
           placeholder="Search"
@@ -29,12 +31,14 @@ const Notices = () => {
           style={{
             borderRadius: '20px',
             backgroundColor: '#FFFFFF',
-            paddingLeft: "20px",
-            paddingRight: "14px",
-            paddingTop: "10px",
-            paddingBottom:"10px",
-           
+            paddingLeft: '20px',
+            paddingRight: '14px',
+            paddingTop: '10px',
+            paddingBottom: '10px',
             boxShadow: '3px 8px 14px rgba(136, 198, 253, 0.19)',
+          }}
+           inputProps={{
+            className: css.placeholder, 
           }}
           endAdornment={
             <InputAdornment position="end">
@@ -45,9 +49,9 @@ const Notices = () => {
           }
           fullWidth
         />
-        </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
-export default Notices
+export default Notices;
