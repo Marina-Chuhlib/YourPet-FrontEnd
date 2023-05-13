722dba884c1113d0fd238930ddaff6b6ce6b21a7
Tatiana-Hela <malya9472@gmail.com>
2023-05-13 13:06:09 +0300

    update notices

--- src/modules/Notices/Notices.jsx
+++ src/modules/Notices/Notices.jsx
@@ -1,10 +1,53 @@
-
+import React, { useState } from 'react';
+import {  InputAdornment, IconButton,Input } from '@mui/material';
+import { Search } from '@mui/icons-material';
 
 const Notices = () => {
-  return (
-    <>
-      <h2>Notices</h2>
-    </>
-  );
-};
-export default Notices;
+  const [keyword, setKeyword] = useState('');
+  
+  // const SearchInput = () => {
+  //   const [keyword, setKeyword] = useState('');
+
+    const handleSearch = () => {
+      // Здесь можно выполнить действия при нажатии на кнопку поиска, например, отправить запрос на сервер
+      console.log('Выполняется поиск по ключевому слову:', keyword);
+    };
+
+    const handleChange = (event) => {
+      setKeyword(event.target.value);
+    };
+
+    return (
+      <>
+        <h2>Find your favorite pet</h2>
+        <div>
+          <Input
+          value={keyword}
+          onChange={handleChange}
+          placeholder="Search"
+          disableUnderline
+          style={{
+            borderRadius: '20px',
+            backgroundColor: '#FFFFFF',
+            paddingLeft: "20px",
+            paddingRight: "14px",
+            paddingTop: "10px",
+            paddingBottom:"10px",
+           
+            boxShadow: '3px 8px 14px rgba(136, 198, 253, 0.19)',
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={handleSearch} style={{ color: '#54ADFF' }}>
+                <Search />
+              </IconButton>
+            </InputAdornment>
+          }
+          fullWidth
+        />
+        </div>
+      </>
+    );
+  };
+
+export default Notices
