c5bacf5b5b3410146bfcd61ad0aa1f5649b428e5
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-05-15 15:25:32 +0300

    update

--- src/modules/UserForm/UserForm.jsx
+++ src/modules/UserForm/UserForm.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
@@ -32,41 +32,77 @@ import Favorite from '@mui/icons-material/Favorite';
 
 const UserForm = () => {
   const { name, email } = useSelector(state => state.auth.user);
-  const [inputValue, setInputValue] = useState('');
-  // const [checked, setChecked] = React.useState(false);
 
-  // console.log(name);
-  // const dispatch = useDispatch();
+  const [formData, setFormData] = useState({
+    name: 'Max',
+    email: 'max@gmail.com',
+    birthday: '',
+    phone: '',
+    city: '',
+  });
 
-  const handleChange = event => {
-    console.log('Hi');
-  };
+  const [editingFields, setEditingFields] = useState({
+    name: false,
+    email: false,
+    birthday: false,
+    phone: false,
+    city: false,
+  });
+
+  const filledFields = {};
 
   const handleChangeInput = event => {
-    setInputValue(event.target.value);
-    console.log(inputValue, 'INPUT');
+    const { name, value } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+    console.log(formData, "'===inputValue'");
   };
+
+  const handleEditField = fieldName => {
+    setEditingFields(prevEditingFields => ({
+      ...prevEditingFields,
+      [fieldName]: !prevEditingFields[fieldName],
+    }));
+
+    const nameValue = editingFields.name;
+    const emailValue = editingFields.email;
+    if (nameValue) {
+      console.log(formData.name, "'= inputValue'", 'DONE');
+    }
+    if (emailValue) {
+      console.log(formData.name, "'= inputValue'", 'DONE');
+    }
+  };
+
+  // отправка на сервер
+  // const filledFields = {};
+  // for (const key in formData) {
+  //   if (formData.hasOwnProperty(key) && formData[key] !== '') {
+  //     filledFields[key] = formData[key];
+  //   }
+  // }
+
+  const fields = [
+    { fieldName: 'name', label: 'Name', type: 'text' },
+    { fieldName: 'email', label: 'Email', type: 'email' },
+    { fieldName: 'birthday', label: 'Birthday', type: 'date' },
+    { fieldName: 'phone', label: 'Phone', type: 'tel' },
+    { fieldName: 'city', label: 'City', type: 'text' },
+  ];
+
+  const initialState = {};
+
+  // console.log(state.name, 'state.name');
+  // const dispatch = useDispatch();
 
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
         <h2 className={css.title}>My information:</h2>
 
-        <Formik
-          initialValues={{
-            name: name,
-            email: email,
-            birthday: '',
-            phone: '',
-            city: '',
-          }}
-  
-          // validationSchema={SignupSchema}
-          onSubmit={values => {
-            // same shape as initial values
-            console.log(values);
-          }}
-        >
+        <Formik>
           {({ errors, touched }) => (
             <Form className={css.forma}>
               <img src="" alt="" className={css.avatar} />
@@ -85,9 +121,58 @@ const UserForm = () => {
                 </label>
               </div>
 
-              {/* //////////////////////////// */}
+              {/* Name*/}
 
-              <div className={css.row}>
+              <div>
+                {fields.map(field => (
+                  <div className={css.row} key={field.fieldName}>
+                    <label className={css.label}>{field.label}:</label>
+
+                    <div className={css.inputContainer}>
+                      <input
+                        name={field.fieldName}
+                        type={field.type}
+                        className={css.input}
+                        value={formData[field.fieldName]}
+                        onChange={handleChangeInput}
+                        disabled={!editingFields[field.fieldName]}
+                      />
+                      {errors[field.fieldName] && touched[field.fieldName] ? (
+                        <div>{errors[field.fieldName]}</div>
+                      ) : null}
+
+                      <div className={css.checkbox}>
+                        <Checkbox
+                          checked={editingFields[field.fieldName]}
+                          onChange={() => handleEditField(field.fieldName)}
+                          //  onChange={handleEdit}
+                          icon={
+                            <BorderColorOutlinedIcon
+                              style={{
+                                color: '#54ADFF',
+                                width: '20px',
+                                height: '20px',
+                              }}
+                            />
+                          }
+                          checkedIcon={
+                            <CheckOutlinedIcon
+                              style={{
+                                color: '#00C3AD',
+                                width: '20px',
+                                height: '20px',
+                                border: '1.5px',
+                              }}
+                            />
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              {/* <div className={css.row}>
                 <label className={css.label}>Name:</label>
 
                 <div className={css.inputContainer}>
@@ -95,8 +180,9 @@ const UserForm = () => {
                     name="name"
                     type="name"
                     className={css.input}
-                    value={inputValue}
+                    value={inputValue.name}
                     onChange={handleChangeInput}
+                    disabled={!isEditing}
                   />
                   {errors.name && touched.name ? (
                     <div>{errors.name}</div>
@@ -104,8 +190,9 @@ const UserForm = () => {
 
                   <div className={css.checkbox}>
                     <Checkbox
-                      // checked={checked}
-                      onChange={handleChange}
+                      checked={isEditing}
+                      onChange={handleEdit}
+                      value={isEditing}
                       icon={
                         <BorderColorOutlinedIcon
                           style={{
@@ -128,21 +215,30 @@ const UserForm = () => {
                     />
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               {/* EMAIL */}
-              <div className={css.row}>
+
+              {/* <div className={css.row}>
                 <label>Email:</label>
                 <div className={css.inputContainer}>
-                  <Field name="email" type="email" className={css.input} />
+                  <Field
+                    name="email"
+                    type="email"
+                    className={css.input}
+                    value={inputValue.email}
+                    onChange={handleChangeInput}
+                    disabled={!isEditing}
+                  />
                   {errors.email && touched.email ? (
                     <div>{errors.email}</div>
                   ) : null}
 
                   <div className={css.checkbox}>
                     <Checkbox
-                      // checked={checked}
-                      onChange={handleChange}
+                      checked={isEditing}
+                      onChange={handleEdit}
+                      value={isEditing}
                       icon={
                         <BorderColorOutlinedIcon
                           style={{
@@ -165,11 +261,11 @@ const UserForm = () => {
                     />
                   </div>
                 </div>
-              </div>
+              </div> */}
 
-              {/* b */}
+              {/* Birthday */}
 
-              <div className={css.row}>
+              {/* <div className={css.row}>
                 <label>Birthday:</label>
                 <div className={css.inputContainer}>
                   <Field name="birthday" className={css.input} />
@@ -203,11 +299,11 @@ const UserForm = () => {
                     />
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               {/* pppp */}
 
-              <div className={css.row}>
+              {/* <div className={css.row}>
                 <label>Phone:</label>
                 <div className={css.inputContainer}>
                   <Field name="phone" className={css.input} />
@@ -241,11 +337,11 @@ const UserForm = () => {
                     />
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               {/* hhhh */}
 
-              <div className={css.row}>
+              {/* <div className={css.row}>
                 <label>City:</label>
                 <div className={css.inputContainer}>
                   <Field name="city" className={css.input} />
@@ -279,7 +375,7 @@ const UserForm = () => {
                     />
                   </div>
                 </div>
-              </div>
+              </div> */}
             </Form>
           )}
         </Formik>
