74a54c57c47ddbe0af802fc7f9d8478228321624
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-05-17 01:34:33 +0300

    do section Main

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -1,8 +1,17 @@
+import css from './main.module.css';
+
 const Main = () => {
   return (
     <>
-      <h2>Main</h2>
-      
+      <section className={css.sectionMain}>
+        <div className="container">
+          <div className={css.positionText}>
+            <h1 className={css.text}>Take good care of your small pets</h1>
+
+            <div className={css.wrapper}></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
