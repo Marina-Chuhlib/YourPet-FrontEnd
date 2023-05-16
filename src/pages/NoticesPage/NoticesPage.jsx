bc8d38791c7e0776ee375c1ae7708fc0fd517ece
Tatiana-Hela <malya9472@gmail.com>
2023-05-16 22:35:17 +0300

    fix Notices

--- src/pages/NoticesPage/NoticesPage.jsx
+++ src/pages/NoticesPage/NoticesPage.jsx
@@ -1,26 +1,23 @@
 import NoticesSearch from 'modules/Notices/NoticesSearch/NoticesSearch';
 import NoticesCategoriesNav from 'modules/Notices/NoticesCategoriesNav/NoticesCategoriesNav';
-import NoticesCategoriesList from 'modules/Notices/NoticesCategoriesList/NoticesCategoriesList';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchNoticesByCategory } from '../../redux/notices/noticesOperations';
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import css from '../NoticesPage/NoticesPage.module.css';
 
 const NoticesPage = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchNoticesByCategory());
-    console.log('Cпрацював useEffect з dispatch');
-  });
+    dispatch(fetchNoticesByCategory({categoryName:"sell", query:""}));
+  },[dispatch]);
   return (
     <>
       <h2 className={css.title}>Find your favorite pet</h2>
       <NoticesSearch />
       <NoticesCategoriesNav />
-      <NoticesCategoriesList />
-      {/* <Outlet /> */}
+      <Outlet />
     </>
   );
 };
