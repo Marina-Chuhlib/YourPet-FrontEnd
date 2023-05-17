0f105a829c4e1d5f50dfcd94e6e74706d0651783
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-05-17 23:07:29 +0300

    add page not-found

--- src/modules/NotFoundRoute/NotFoundRoute.jsx
+++ src/modules/NotFoundRoute/NotFoundRoute.jsx
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+import css from './not-found-route.module.css';
 
-const NotFoundRoute = ()=> {
-    return <h2>Not found page</h2>
-}
+const NotFoundRoute = () => {
+  return (
+    <section className={css.section}>
+      <div className="container">
+        <h2 className={css.text}>Ooops! This page not found:(</h2>
+        <div className={css.wrapper}></div>
+        <Link to="/" className={css.link}>
+          To main page
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default NotFoundRoute;
