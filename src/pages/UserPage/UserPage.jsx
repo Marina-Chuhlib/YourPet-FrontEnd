d03b811fc21a76f1d985667744c6b7c772fd27ec
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-05-19 13:43:27 +0300

    Update UserPage.jsx

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,15 +1,45 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import UserCard from 'modules/UserForm/UserCard';
+import ModalCongrats from 'shared/components/ModalCongrats/ModalCongrats';
 import { current } from 'redux/auth/auth-operations';
+import {
+  selectIsLoggedIn,
+  selectIsLoading,
+  selectRegistrationSuccessful,
+} from 'redux/auth/auth-selectors';
 
 const UserPage = () => {
+  const [showModal, setShowModal] = useState(false);
 
-current()
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isLoading = useSelector(selectIsLoading);
+  const registrationSuccessful = useSelector(selectRegistrationSuccessful);
+
+  current();
+
+  // const dispatch = useDispatch()
+
   // useEffect(() => {
   //   dispatch(current());
   // }, [dispatch]);
 
+  useEffect(() => {
+    if (!isLoading && isLoggedIn && registrationSuccessful) {
+      setShowModal(true);
+    }
+  }, [isLoading, isLoggedIn, registrationSuccessful]);
 
-  return <UserCard />;
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      {showModal && <ModalCongrats closeModal={closeModal} />}
+      <UserCard />;
+    </>
+  );
 };
 
 export default UserPage;
