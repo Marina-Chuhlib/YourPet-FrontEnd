3e57507c20e7351859f00b72e4af394a1417b87d
Olena <newsroommy@gmail.com>
2023-05-19 16:39:18 +0300

    fix

--- src/modules/Notices/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/modules/Notices/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -13,14 +13,6 @@ import useToggleModalWindow from 'shared/hooks/useToggleModalWindow';
 import Modal from 'shared/components/ModalWindow/Modal';
 import NoticeModal from 'modules/NoticeModal/NoticeModal';
 
-// import useToggleModalWindow from 'shared/hooks/useToggleModalWindow';
-// import Modal from 'shared/components/ModalWindow/Modal';
-// import NoticeModal from 'modules/NoticeModal/NoticeModal';
-
-// import useToggleModalWindow from 'shared/hooks/useToggleModalWindow';
-// import Modal from 'shared/components/ModalWindow/Modal';
-// import NoticeModal from 'modules/NoticeModal/NoticeModal';
-
 import css from './notice-categories-item.module.css';
 
 const NoticeCategoryItem = ({
