f55dfc6bfd645d10dcba2b34f91ecef5c48a4608
Oksana1594 <106923229+Oksana1594@users.noreply.github.com>
2023-05-20 15:14:28 +0300

    change style in main page and not-found page

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -1,14 +1,38 @@
 import css from './main.module.css';
 
+import dogsMobile1x from 'images/dogs-mobile1x.png'
+import dogsMobile2x from 'images/dogs-mobile2x.png'
+import dogsTat1x from 'images/dogs-tablet1x.png'
+import dogsTab2x from 'images/dogs-tablet2x.png'
+import dogsDesc1x from 'images/dogs-desktop@1x.png'
+import dogsDesc2x from 'images/dogs-desktop2x.png'
+
 const Main = () => {
   return (
     <>
       <section className={css.sectionMain}>
         <div className="container">
           <div className={css.positionText}>
-            <h1 className={css.text}>Take good care of your small pets</h1>
+            <div><h1 className={css.text}>Take good care of your small pets</h1></div>
+            
 
-            <div className={css.wrapper}></div>
+            <div className={css.wrapper}>
+              <picture>
+            <source
+              media="(min-width: 1280px)"
+              srcSet={`${dogsDesc1x}, ${dogsDesc2x} 2x`}
+            />
+            <source
+              media="(min-width: 768px)"
+              srcSet={`${dogsTat1x}, ${dogsTab2x} 2x`}
+            />
+            <img
+              src={dogsMobile1x}
+              srcSet={`${dogsMobile1x}, ${dogsMobile2x} 2x`}
+              alt="Dogs and cat"
+            />
+          </picture>
+            </div>
           </div>
         </div>
       </section>
--- src/modules/NotFoundRoute/NotFoundRoute.jsx
+++ src/modules/NotFoundRoute/NotFoundRoute.jsx
@@ -1,12 +1,35 @@
 import { Link } from 'react-router-dom';
 import css from './not-found-route.module.css';
 
+import oopsMobile1x from 'images/oops-mob@1x.png'
+import oopsMobile2x from 'images/oops-mob@2x.png'
+import oopsTab1x from 'images/oops-tab@1x.png'
+import oopsTab2x from 'images/oops-tab@2x.png'
+import oopsDesc1x from 'images/oops-desc@1x.png'
+import oopsDesc2x from 'images/oops-desc@2x.png'
+
 const NotFoundRoute = () => {
   return (
     <section className={css.section}>
       <div className="container">
         <h2 className={css.text}>Ooops! This page not found:(</h2>
-        <div className={css.wrapper}></div>
+        <div className={css.wrapper}>
+          <picture>
+            <source
+              media="(min-width: 1280px)"
+              srcSet={`${oopsDesc1x}, ${oopsDesc2x} 2x`}
+            />
+            <source
+              media="(min-width: 768px)"
+              srcSet={`${oopsTab1x}, ${oopsTab2x} 2x`}
+            />
+            <img
+              src={oopsMobile1x}
+              srcSet={`${oopsMobile1x}, ${oopsMobile2x} 2x`}
+              alt="Dogs and cat"
+            />
+          </picture>
+        </div>
         <Link to="/" className={css.link}>
           To main page
         </Link>
