e4d4ef0792df383d78a0adaf6c093648b3d9af44
shatniydmitriy <pp16dima10@gmail.com>
2023-05-21 20:04:22 +0300

    add

--- src/modules/Header/NavBarAuth/NavBarAuth.jsx
+++ src/modules/Header/NavBarAuth/NavBarAuth.jsx
@@ -1,13 +1,24 @@
 import { NavLink } from "react-router-dom";
-
+import PawprintIcon from "../../../icons/PawPrintIcon";
 import styles from "./navbarauth.module.css"
 
 const NavBarAuth = () => {
     return (
-        <div className={styles.wraper}>
-            <NavLink to="/register" className={styles.link}><button>Register</button></NavLink>
-            <NavLink to="/login" className={styles.link}><button>Login</button></NavLink>
-    </div>
+        <nav className={styles.nav}>
+        <ul className={styles.list}>
+            <li className={styles.item}>
+            <NavLink className={`${styles.link} ${styles.login}`} to="/login">
+                Log IN
+                <PawprintIcon className={styles.icon} />
+            </NavLink>
+            </li>
+            <li className={styles.item}>
+            <NavLink className={styles.link} to="/register">
+                Registration
+            </NavLink>
+            </li>
+        </ul>
+        </nav>
     )
 
 }
