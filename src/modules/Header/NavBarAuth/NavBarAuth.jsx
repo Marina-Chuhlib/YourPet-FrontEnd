2d6c4b07884719ff095ef6d38d13179855cf2575
shatniydmitriy <pp16dima10@gmail.com>
2023-05-22 20:37:44 +0300

    fix

--- src/modules/Header/NavBarAuth/NavBarAuth.jsx
+++ src/modules/Header/NavBarAuth/NavBarAuth.jsx
@@ -2,18 +2,18 @@ import { NavLink } from "react-router-dom";
 import PawprintIcon from "../../../icons/PawPrintIcon";
 import styles from "./navbarauth.module.css"
 
-const NavBarAuth = () => {
+const NavBarAuth = (onClick) => {
     return (
         <nav className={styles.nav}>
         <ul className={styles.list}>
             <li className={styles.item}>
-            <NavLink className={`${styles.link} ${styles.login}`} to="/login">
+            <NavLink className={`${styles.link} ${styles.login}`} to="/login" onClick={onClick}>
                 Log IN
                 <PawprintIcon className={styles.icon} />
             </NavLink>
             </li>
             <li className={styles.item}>
-            <NavLink className={styles.link} to="/register">
+            <NavLink className={styles.link} to="/register" onClick={onClick}>
                 Registration
             </NavLink>
             </li>
