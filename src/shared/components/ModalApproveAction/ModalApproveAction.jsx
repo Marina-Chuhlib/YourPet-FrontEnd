2674ff6c50274f8c1106571cdc90dea200a19ab4
IraGubish2022 <iragubish@gmail.com>
2023-05-25 20:44:40 +0300

    форматування коду

--- src/shared/components/ModalApproveAction/ModalApproveAction.jsx
+++ src/shared/components/ModalApproveAction/ModalApproveAction.jsx
@@ -1,37 +1,39 @@
 import Modal from '../ModalWindow/Modal';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-//import Button from '@mui/material/Button';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import css from './ModalApproveAction.module.css';
 import { logout } from 'redux/auth/auth-operations';
 
 const ModalApproveAction = ({ closeModal }) => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleModalClose = () => {
-        closeModal();
-        navigate('/user');
-      };
+  const handleModalClose = () => {
+    closeModal();
+    navigate('/user');
+  };
 
-      const handleLogout = () => {
-        closeModal();
-        dispatch(logout());
-        navigate('/');
+  const handleLogout = () => {
+    closeModal();
+    dispatch(logout());
+    navigate('/');
   };
 
   return (
     <>
       <Modal className={css.modalApprove} closeModal={handleModalClose}>
-        <h1>Already leaving?</h1>
+        <h2 className={css.modalTitle}>Already leaving?</h2>
         <div className={css.modalBtnContainer}>
-        <button className={`${css.modalBtn} ${css.whiteBtn}`} onClick={handleModalClose}>
-          Cancel
-        </button>
-        <button className={css.modalBtn} onClick={handleLogout}>
-          Yes <LogoutOutlinedIcon className={css.modalIcon} />
-        </button>
+          <button
+            className={`${css.modalBtn} ${css.whiteBtn}`}
+            onClick={handleModalClose}
+          >
+            Cancel
+          </button>
+          <button className={css.modalBtn} onClick={handleLogout}>
+            Yes <LogoutOutlinedIcon className={css.modalIcon} />
+          </button>
         </div>
       </Modal>
     </>
