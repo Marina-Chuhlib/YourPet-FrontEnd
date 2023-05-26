7f96be82379e05043645c37607a088808f5a977e
Marina-Chuhlib <marinachuhlib@gmail.com>
2023-05-26 13:35:13 +0300

    add REACT_APP_BASE_URL

--- src/shared/services/App/app.js
+++ src/shared/services/App/app.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
-// const { REACT_APP_BASE_URL } = process.env;
-
-// const instance = axios.create({
-//   baseURL: REACT_APP_BASE_URL,
-// });
+const { REACT_APP_BASE_URL } = process.env;
 
 const instance = axios.create({
-  baseURL: 'https://yourpet-backend-3yf8.onrender.com/api',
+  baseURL: REACT_APP_BASE_URL,
 });
 
 export const setToken = token => {
